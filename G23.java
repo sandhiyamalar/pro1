f779150a00e82f3ffa148024755c0bb326f7c9c0
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-08-23 11:55:21 +0530

    G23.java

---
+++ G23.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+public class G23{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=in.nextInt();
+        }
+        Arrays.sort(a);
+        for(int i=0;i<n;i++){
+           System.out.print(a[i]+" ");
+        }
+    }
+}
