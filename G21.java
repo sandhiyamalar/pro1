e9ded5554b52840daac0cea6bc293ec274c10f39
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-08-23 11:47:19 +0530

    G21.java

---
+++ G21.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class G21{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int[] a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=in.nextInt();
+        }
+        int max=a[0];
+        for(int i=0;i<n;i++){
+            if(a[i]>max)
+               max=a[i];
+        }
+        System.out.print(max);
+    }
+}
+
