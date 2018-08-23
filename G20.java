6b702354a12435ee794bcb4bb5220c693f11b248
sandhiyamalar <40415790+sandhiyamalar@users.noreply.github.com>
2018-08-23 11:12:00 +0530

    G20.java

---
+++ G20.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class G20{
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        for(int i=1;i<=5;i++){
+            System.out.print(i*n+" ");
+        }
+    }
+}
+
